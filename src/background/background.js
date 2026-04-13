0fd7e07596774064073241c895c68586f6581cca
baw <mail@baweibel.ch>
2026-04-13 12:49:36 +0200

    v1.0.1

--- src/background/background.js
+++ src/background/background.js
@@ -318,7 +318,7 @@ function buildRequestBody(url, config) {
     return {
       client: {
         clientId: 'warnlisten-plugin',
-        clientVersion: '1.0.0'
+        clientVersion: '1.0.1'
       },
       threatInfo: {
         threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE', 'POTENTIALLY_HARMFUL_APPLICATION'],
